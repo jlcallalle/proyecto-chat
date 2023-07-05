99322cfe247b18fcb7a8c398a6ce4b1b18d7bc36
Jorge Callalle <jorge.callalle@vasslatam.com>
2023-07-05 18:07:26 -0500

    completa chat con socket.io y cookies

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -4,13 +4,15 @@ const send = document.querySelector("#send-message");
 const allMessages = document.querySelector("#all-messages");
 
 send.addEventListener("click", () => {
-    const message = document.querySelector("#message").value;
-    socket.emit("message", message);
+    const message = document.querySelector("#message");
+    socket.emit("message", message.value);
+
+    message.value = "";
 });
 
 socket.on("message", ({user, message}) => {
 
-    const msg = document.createRange().createContextualFragment(`
+    const msg = document.createRange().createContextualFragment(/*html*/`
     <div class="message">
                 
         <div class="image-container">
--- src/realtimeServer.js
+++ src/realtimeServer.js
@@ -6,11 +6,14 @@ module.exports = httpServer => {
     io.on("connection", socket => {
 
         // console.log(socket.id);
+        // accerder a cookie desde socket.io
+        const cookie = socket.handshake.headers.cookie;
+        const user = cookie.split("=").pop();
+        console.log(user);
         socket.on("message", message => {
 
             io.emit("message", {
-                user: "Jorge",
-                message
+                user, message
             });
     
            })
